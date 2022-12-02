4706dc25396aac9e25caf3480312bfb8caaa53e8
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2022-12-02 16:38:55 +0700

    first app

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get('/')
+def index():
+    return {"data": {"name" : "Rizal Fadia Al Fikri"}}
+
+@app.get('/about/')
+def about():
+    return {"data" : 'about page'}
