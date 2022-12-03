08d0368e68fe00225a73d14fa4a0bfef94ee1465
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2022-12-03 14:37:39 +0700

    Query Parameters

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {"data": "Blog list"}
-
+@app.get('/blog/')
+def index(limit = 10, published : bool = True, sort: Optional[str] = None):
+    if published:
+        return {"data": f"{limit} published Blogs from the db"}
+    else:
+        return {"data": f"{limit} Blogs from the db"}
 @app.get('/blog/unpublished')
 def unpublished():
     return {'data' : 'all unpublished blogs'}
