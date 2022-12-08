0d611dfc9ce7bfded17013fe001d825dd6e9635a
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2022-12-08 13:56:05 +0700

    update a blog

--- blog/main.py
+++ blog/main.py
@@ -36,6 +36,18 @@ def destroy(id, db: Session = Depends(get_db)):
     return 'done'
 
 
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with the id {id} is not available')
+
+    blog.update(request)
+    db.commit()
+    return 'updated'
+
+
 @app.get('/blog')
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
